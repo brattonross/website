71cb5977caae2b6db99c32a9e7b6c7f63d7dc997
Ross Bratton <bratton.ross@gmail.com>
2023-09-02 16:01:00 +0100

    use radix themes style text

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,72 @@ export default {
 					...defaultTheme.fontFamily.sans,
 				],
 			},
+			// Based on https://github.com/radix-ui/themes/blob/main/packages/radix-ui-themes/src/styles/tokens/typography.css
+			fontSize: {
+				"5xl": [
+					"60px",
+					{
+						letterSpacing: "-0.025em",
+						lineHeight: "60px",
+					},
+				],
+				"4xl": [
+					"35px",
+					{
+						letterSpacing: "-0.01em",
+						lineHeight: "40px",
+					},
+				],
+				"3xl": [
+					"28px",
+					{
+						letterSpacing: "-0.0075em",
+						lineHeight: "36px",
+					},
+				],
+				"2xl": [
+					"24px",
+					{
+						letterSpacing: "-0.00625em",
+						lineHeight: "30px",
+					},
+				],
+				xl: [
+					"20px",
+					{
+						letterSpacing: "-0.005em",
+						lineHeight: "28px",
+					},
+				],
+				lg: [
+					"18px",
+					{
+						letterSpacing: "-0.0025em",
+						lineHeight: "26px",
+					},
+				],
+				base: [
+					"16px",
+					{
+						letterSpacing: "0em",
+						lineHeight: "24px",
+					},
+				],
+				sm: [
+					"14px",
+					{
+						letterSpacing: "0em",
+						lineHeight: "20px",
+					},
+				],
+				xs: [
+					"12px",
+					{
+						letterSpacing: "0.0025em",
+						lineHeight: "16px",
+					},
+				],
+			},
 		},
 	},
 	plugins: [
