d5e6e29d3adc3e96e4a097bdc4eee7065038598f
Ross Bratton <bratton.ross@gmail.com>
2023-09-02 16:31:24 +0100

    refactor theme stuff

---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+export const THEMES = ["auto", "light", "dark"] as const;
+
+export type Theme = typeof THEMES[number];
+
+export function isTheme(theme: unknown): theme is Theme {
+  return THEMES.includes(theme as Theme);
+}
+
+export const THEME_KEY = "brattonross_theme" as const;
+
+export function humanizeTheme(theme: Theme): string {
+  switch (theme) {
+	case "light":
+	  return "Light";
+	case "dark":
+	  return "Dark";
+	case "auto":
+	  return "Auto";
+  }
+}
+
+export function getThemeOrDefault(theme: unknown): Theme {
+  if (isTheme(theme)) {
+	return theme;
+  }
+  return "auto";
+}
