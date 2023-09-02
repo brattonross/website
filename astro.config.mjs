a06d69d6b46c77158f4c69c7d09d67bf4adf4c3a
Ross Bratton <bratton.ross@gmail.com>
2023-09-02 13:41:05 +0100

    dockerfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
+	adapter: node({
+		mode: "standalone",
+	}),
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
