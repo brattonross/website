9bd3bc58023f5d3da5bccd2fb3d134f1ddb691c0
Ross Bratton <bratton.ross@gmail.com>
2023-10-04 21:07:34 +0100

    redo some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,79 +7,18 @@ export default {
 	darkMode: "class",
 	theme: {
 		extend: {
+			boxShadow: {
+				b: "0 1px var(--tw-shadow-color)",
+				t: "0 -1px var(--tw-shadow-color)",
+			},
 			colors: {
 				"hi-contrast": "var(--text-hi-contrast)",
 				"lo-contrast": alias("sand", 11),
-				"focus-ring": alias("jade", 9),
+				gray: alias("sand"),
+				grayA: alias("sandA"),
 			},
 			fontFamily: {
 				sans: ["Atkinson Hyperlegible", ...defaultTheme.fontFamily.sans],
-			},
-			// Based on https://github.com/radix-ui/themes/blob/main/packages/radix-ui-themes/src/styles/tokens/typography.css
-			fontSize: {
-				"5xl": [
-					"60px",
-					{
-						letterSpacing: "-0.025em",
-						lineHeight: "60px",
-					},
-				],
-				"4xl": [
-					"35px",
-					{
-						letterSpacing: "-0.01em",
-						lineHeight: "40px",
-					},
-				],
-				"3xl": [
-					"28px",
-					{
-						letterSpacing: "-0.0075em",
-						lineHeight: "36px",
-					},
-				],
-				"2xl": [
-					"24px",
-					{
-						letterSpacing: "-0.00625em",
-						lineHeight: "30px",
-					},
-				],
-				xl: [
-					"20px",
-					{
-						letterSpacing: "-0.005em",
-						lineHeight: "28px",
-					},
-				],
-				lg: [
-					"18px",
-					{
-						letterSpacing: "-0.0025em",
-						lineHeight: "26px",
-					},
-				],
-				base: [
-					"16px",
-					{
-						letterSpacing: "0em",
-						lineHeight: "24px",
-					},
-				],
-				sm: [
-					"14px",
-					{
-						letterSpacing: "0em",
-						lineHeight: "20px",
-					},
-				],
-				xs: [
-					"12px",
-					{
-						letterSpacing: "0.0025em",
-						lineHeight: "16px",
-					},
-				],
 			},
 		},
 	},
