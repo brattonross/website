75723a6fe50186c2e11013f4018d2772417f5619
Ross Bratton <bratton.ross@gmail.com>
2024-02-15 22:48:31 +0000

    trim down styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,48 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 
+/** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.astro"],
 	darkMode: "class",
 	theme: {
-		extend: {
-			boxShadow: {
-				b: "0 1px var(--tw-shadow-color)",
-				t: "0 -1px var(--tw-shadow-color)",
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				md: "1.5rem",
+				lg: "2rem",
 			},
+		},
+		extend: {
 			colors: {
-				"hi-contrast": colors.alias({
-					light: "sand.12",
-					dark: "#d3c6aa",
-				}),
-				"lo-contrast": colors.alias("sand.11"),
-				gray: colors.alias("sand"),
-				grayA: colors.alias("sandA"),
+				gray: {
+					1: "var(--gray-1)",
+					2: "var(--gray-2)",
+					3: "var(--gray-3)",
+					4: "var(--gray-4)",
+					5: "var(--gray-5)",
+					6: "var(--gray-6)",
+					7: "var(--gray-7)",
+					8: "var(--gray-8)",
+					9: "var(--gray-9)",
+					10: "var(--gray-10)",
+					11: "var(--gray-11)",
+					12: "var(--gray-12)",
+					a1: "var(--gray-a1)",
+					a2: "var(--gray-a2)",
+					a3: "var(--gray-a3)",
+					a4: "var(--gray-a4)",
+					a5: "var(--gray-a5)",
+					a6: "var(--gray-a6)",
+					a7: "var(--gray-a7)",
+					a8: "var(--gray-a8)",
+					a9: "var(--gray-a9)",
+					a10: "var(--gray-a10)",
+					a11: "var(--gray-a11)",
+					a12: "var(--gray-a12)",
+				},
+				"hi-contrast": "var(--gray-12)",
+				"lo-contrast": "var(--gray-11)",
 			},
 			fontFamily: {
 				sans: [
@@ -101,5 +127,5 @@ export default {
 			},
 		},
 	},
-	plugins: [colors.plugin, require("@tailwindcss/typography")],
-} satisfies Config;
+	plugins: [require("@tailwindcss/typography")],
+};
